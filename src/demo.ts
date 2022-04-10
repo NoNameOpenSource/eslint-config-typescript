722f07a0538fe340065fff0519ed3558b1da0d23
Matt Dean <mdean400@gmail.com>
2022-04-10 15:28:59 -0600

    fix(demo): add another demo function

--- src/demo.ts
+++ src/demo.ts
@@ -1,4 +1,11 @@
+import console from "console";
+
 export function throwLiteral() {
     // eslint-disable-next-line @typescript-eslint/no-throw-literal
     throw "literal";
 }
+
+// eslint-disable-next-line @getify/proper-arrows/where
+export const topLevelFatArrowFunction = (arg0: string) => {
+    console.log(arg0);
+};
